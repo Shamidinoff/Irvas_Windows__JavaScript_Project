f99c5ca19e9ac23bb3b99ca3025cfde637dc50b4
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-16 14:51:53 +0600

    Irvas Windows - JavaScript project

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import "./slider";
 import modals from "./modules/modals";
 import tabs from "./modules/tabs";
+import forms from "./modules/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
   modals();
+
   tabs(".glazing_slider ", ".glazing_block", ".glazing_content", "active");
   tabs(
     ".decoration_slider",
@@ -11,4 +14,6 @@ window.addEventListener("DOMContentLoaded", () => {
     ".decoration_content > div > div",
     "after_click"
   );
+
+  forms();
 });
