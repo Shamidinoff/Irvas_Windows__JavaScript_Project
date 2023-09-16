a20783e08640518cf8e7fa3a77a498ed2adeca4e
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-16 16:05:47 +0600

    Irvas Windows - JavaScript project

--- dist/script.js
+++ dist/script.js
@@ -22,10 +22,26 @@ const changeModalState = state => {
   function bindActionToElems(event, elem, prop) {
     elem.forEach((item, i) => {
       item.addEventListener(event, () => {
-        if (elem.length > 1) {
-          state[prop] = i;
-        } else {
-          state[prop] = item.value;
+        switch (item.nodeName) {
+          case "SPAN":
+            state[prop] = i;
+            break;
+          case "INPUT":
+            if (item.getAttribute("type") === "checkbox") {
+              i === 0 ? state[prop] = "Холодное" : state[prop] = "Теплое";
+              elem.forEach((box, j) => {
+                box.checked = false;
+                if (i == j) {
+                  box.checked = true;
+                }
+              });
+            } else {
+              state[prop] = item.value;
+            }
+            break;
+          case "SELECT":
+            state[prop] = item.value;
+            break;
         }
       });
     });
