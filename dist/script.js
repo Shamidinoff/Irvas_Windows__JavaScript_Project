476dcf9b7a05cac48cd1c91965d023ef19973e80
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-16 12:39:33 +0600

    Irvas Windows - JavaScript project

--- dist/script.js
+++ dist/script.js
@@ -16,11 +16,20 @@ const modals = () => {
         e.preventDefault();
       }
       modal.style.display = "block";
-      document.body.style.overflow = "hidden";
+      //   document.body.style.overflow = "hidden";
+      document.body.classList.add("modal-open");
     });
     close.addEventListener("click", () => {
       modal.style.display = "none";
-      document.body.style.overflow = "";
+      //   document.body.style.overflow = "";
+      document.body.classList.remove("modal-open");
+    });
+    modal.addEventListener("click", e => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        // document.body.style.overflow = "";
+        document.body.classList.remove("modal-open");
+      }
     });
   }
   const callEngineerBtn = document.querySelector(".popup_engineer_btn"),
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -6,12 +6,22 @@ const modals = () => {
       }
 
       modal.style.display = "block";
-      document.body.style.overflow = "hidden";
+      //   document.body.style.overflow = "hidden";
+      document.body.classList.add("modal-open");
     });
 
     close.addEventListener("click", () => {
       modal.style.display = "none";
-      document.body.style.overflow = "";
+      //   document.body.style.overflow = "";
+      document.body.classList.remove("modal-open");
+    });
+
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        // document.body.style.overflow = "";
+        document.body.classList.remove("modal-open");
+      }
     });
   }
 
